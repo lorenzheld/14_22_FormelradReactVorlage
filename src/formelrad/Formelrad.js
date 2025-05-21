bdd25c42fd719148e293465d9a94ea0ad78792b2
Lorenz Held <held.lorenz.08@gmail.com>
2025-05-21 09:27:27 +0200

    mergeConflict resolved

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -1,22 +1,27 @@
+import { useState } from "react";
+import '../css/mvp.css';
 import formelrad from "../image/formelradelektronik.gif";
+import InputField from "../formular/InputField";
 
 export default function Formelrad() {
+    const [values, setValues] = useState({
+        u: 10,
+        i: 2,
+        r: ""
+    });
+
     return (
-        <>
-            <h2>Formelrad</h2>
-            <img src={formelrad} width="200" alt="Formelrad"/>
+        <section>
+            <header>
+                <h2>Formelrad</h2>
+                <img src={formelrad} width="200" alt="Formelrad" />
+            </header>
             <form>
-                <div>
-                    <label>Spannung</label><input></input>
-                </div>
-                <div>
-                    <label>Strom</label><input></input>
-                </div>
-                <div>
-                    <label>Widerstand</label><input></input>
-                </div>
+                <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => { setValues(values => ({ ...values, u: e.target.value })) }} />
+                <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => { setValues(values => ({ ...values, i: e.target.value })) }} />
+                <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => { setValues(values => ({ ...values, r: e.target.value })) }} />
                 <button type="submit">Calculate</button>
             </form>
-        </>
-    )
+        </section>
+    );
 }
